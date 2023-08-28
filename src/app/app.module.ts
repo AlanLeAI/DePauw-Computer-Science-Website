b5169bdc83becae82b15886ea202ad24975cad7c
Tuan Le <tuanle_2024@depauw.edu>
2023-08-28 13:47:56 -0400

    Add all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavComponent } from './nav/nav.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    NavComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
